ea6f02b04c97258073af19ba168d86153437f37d
Saqib Arif <saqibarif@hotmail.com>
2019-09-16 04:34:56 +0100

    Expand basket tests

--- src/test/java/com/ford/henrysgroceries/BasketTest.java
+++ src/test/java/com/ford/henrysgroceries/BasketTest.java
@@ -1,21 +1,40 @@
 package com.ford.henrysgroceries;
 
+import com.ford.henrysgroceries.offers.BuyTwoSoupsGetBreadHalfPriceOffer;
+import com.ford.henrysgroceries.offers.Offer;
+import com.ford.henrysgroceries.offers.TenPercentOffApplesOffer;
 import com.ford.henrysgroceries.products.Product;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.time.*;
 import java.util.Arrays;
+import java.util.List;
 
 import static com.ford.henrysgroceries.products.ProductHelper.*;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class BasketTest {
+    private Clock fixedClock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
     private Basket basket;
+    private Offer applesOffer = new TenPercentOffApplesOffer(LocalDate.now());
+    private Offer breadOffer = new BuyTwoSoupsGetBreadHalfPriceOffer(LocalDate.now());
+    private List<Offer> offers = Arrays.asList(applesOffer, breadOffer);
 
     @Test
-    public void emptyBasketTotalIsZero() {
+    public void emptyBasketWithNoOffersTotalsToZero() {
         basket = new Basket();
+
+        BigDecimal total = basket.calculateTotal();
+
+        assertThat(total.compareTo(BigDecimal.ZERO), is(0));
+    }
+
+    @Test
+    public void emptyBasketWithOffersTotalsToZero() {
+        basket = new Basket(offers);
 
         BigDecimal total = basket.calculateTotal();
 
@@ -54,15 +73,70 @@ public class BasketTest {
         assertThat(total.compareTo(new BigDecimal("1.50")), is(0));
     }
 
+    @Test
+    public void threeTinsOfSoupAndTwoLoavesOfBreadBoughtTodayCosts3Pounds15Pence() {
+        givenBasketHasProducts(
+                soup(), soup(), soup(),
+                bread(), bread()
+        );
+
+        BigDecimal total = basket.calculateTotal();
+
+        assertThat(total.compareTo(new BigDecimal("3.15")), is(0));
+    }
+
+    @Test
+    public void sixApplesAndOneBottleOfMilkBoughtTodayCosts1Pound90Pence() {
+        givenBasketHasProducts(
+                apples(), apples(), apples(),
+                apples(), apples(), apples(),
+                milk()
+        );
+
+        BigDecimal total = basket.calculateTotal();
+
+        assertThat(total.compareTo(new BigDecimal("1.90")), is(0));
+    }
+
+    @Test
+    public void sixApplesAndOneBottleOfMilkBoughtIn5DaysTimeCosts1Pound84Pence() {
+        Clock clock = Clock.offset(fixedClock, Duration.ofDays(5));
+        givenBasketHasProducts(
+                clock,
+                apples(), apples(), apples(),
+                apples(), apples(), apples(),
+                milk()
+        );
+
+        BigDecimal total = basket.calculateTotal();
+
+        assertThat(total.compareTo(new BigDecimal("1.84")), is(0));
+    }
+
+    @Test
+    public void threeApplesAndTwoTinsOfSoupAndAndOneLoafOfBreadBoughtIn5DaysTimeCosts1Pound97Pence() {
+        Clock clock = Clock.offset(fixedClock, Duration.ofDays(5));
+        givenBasketHasProducts(
+                clock,
+                apples(), apples(), apples(),
+                soup(), soup(),
+                bread()
+        );
+
+        BigDecimal total = basket.calculateTotal();
+
+        assertThat(total.compareTo(new BigDecimal("1.97")), is(0));
+    }
+
     private void givenBasketHasProduct(Product product) {
         givenBasketHasProducts(product);
     }
 
     private void givenBasketHasProducts(Product... products) {
-        basket = new Basket();
+        givenBasketHasProducts(fixedClock, products);
+    }
 
-        for (Product product : products) {
-            basket.addProduct(product);
-        }
+    private void givenBasketHasProducts(Clock clock, Product... products) {
+        basket = new Basket(offers, clock, products);
     }
 }
