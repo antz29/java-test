fb7fc51d544f591f439f9c3c6f6e2a2c7d9d87b6
Saqib Arif <saqibarif@hotmail.com>
2019-09-16 02:36:38 +0100

    Minor refactors to TenPercentOffApplesOffer

--- src/main/java/com/ford/henrysgroceries/offers/TenPercentOffApplesOffer.java
+++ src/main/java/com/ford/henrysgroceries/offers/TenPercentOffApplesOffer.java
@@ -11,14 +11,10 @@ import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 
 public class TenPercentOffApplesOffer implements Offer {
 
-    private final Product discountedProduct;
-    private final BigDecimal percentage;
     private final LocalDate start;
     private final LocalDate end;
 
     public TenPercentOffApplesOffer(LocalDate today) {
-        discountedProduct = apples();
-        percentage = new BigDecimal(10);
         start = today.plusDays(3);
         end = today.plusMonths(1).with(lastDayOfMonth());
     }
@@ -29,7 +25,7 @@ public class TenPercentOffApplesOffer implements Offer {
             return basket;
 
         basket.getProducts().stream()
-                .filter(product -> product.getName().equals(discountedProduct.getName()))
+                .filter(product -> product.getName().equals(apples().getName()))
                 .forEach(product -> product.setDiscountPrice(setDiscountPrice(product)));
         return basket;
     }
@@ -40,6 +36,7 @@ public class TenPercentOffApplesOffer implements Offer {
 
     private BigDecimal setDiscountPrice(Product product) {
         BigDecimal price = product.getPrice();
+        BigDecimal percentage = new BigDecimal(10);
         BigDecimal discount = price.divide(new BigDecimal("100.00")).multiply(percentage);
         return price.subtract(discount);
     }
