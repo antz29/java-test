b0f1858b882b7f5eb5bea9a862f8e667871232c9
jidefakoya <jidefakoya@hotmail.co.uk>
2019-12-26 22:17:18 +0000

    Add serialVersionUID to InvalidItemException

--- src/main/java/com/jfsoftware/henrys/InvalidItemException.java
+++ src/main/java/com/jfsoftware/henrys/InvalidItemException.java
@@ -1,6 +1,8 @@
 package com.jfsoftware.henrys;
 
 class InvalidItemException extends RuntimeException {
+    private static final long serialVersionUID = 1282542169604438101L;
+
     InvalidItemException(String message) {
         super(message);
     }
