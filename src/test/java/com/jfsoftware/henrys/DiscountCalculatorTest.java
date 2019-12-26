a02b02a1a0127be6d1b5577444037a84cbecd96b
jidefakoya <jidefakoya@hotmail.co.uk>
2019-12-26 21:06:48 +0000

    Implement discount calculator test

--- src/test/java/com/jfsoftware/henrys/DiscountCalculatorTest.java
+++ src/test/java/com/jfsoftware/henrys/DiscountCalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static com.jfsoftware.henrys.Product.APPLE;
+import static com.jfsoftware.henrys.Product.*;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,5 +30,70 @@ public class DiscountCalculatorTest {
                 new StockItem(APPLE),
                 new StockItem(APPLE)));
         assertThat(discount).isEqualTo(new BigDecimal("0.02"));
+    }
+
+    @Test
+    void breadIsNotDiscountedWhenThereIsNoSoup() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(singletonList(new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.00"));
+    }
+
+    @Test
+    void breadIsNotDiscountedWhenThereIsOnlyOneSoup() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(asList(
+                new StockItem(SOUP),
+                new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.00"));
+    }
+
+    @Test
+    void twoSoupAndOneLoafAreDiscountedCorrectly() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(asList(
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.40"));
+    }
+
+    @Test
+    void threeSoupAndOneLoafAreDiscountedCorrectly() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(asList(
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.40"));
+    }
+
+    @Test
+    void threeSoupAndTwoLoavesAreDiscountedCorrectly() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(asList(
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(BREAD),
+                new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.40"));
+    }
+
+    @Test
+    void twoSoupAndTwoLoavesAreDiscountedCorrectly() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(asList(
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(BREAD),
+                new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.40"));
+    }
+
+    @Test
+    void fourSoupAndOneLoafAreDiscountedCorrectly() {
+        BigDecimal discount = calculator.calculateSoupBreadDiscount(asList(
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(SOUP),
+                new StockItem(BREAD)));
+        assertThat(discount).isEqualTo(new BigDecimal("0.80"));
     }
 }
